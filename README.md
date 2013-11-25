b4d2b77e615d1205e72d9be8767f05e5a3ed29f9
Murray Colpman <muzer@tim32.org>
2013-11-25 22:07:50 +0000

    Add index.php file. Update readme.
    Add index.php - the index menu. Add info about it just being a menu to
    the README.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ website
 =======
 
 The Timlan website. Not really very much at the moment.
+
+Currently just a menu.
---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+
+<title>Timlan</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+
+<h1>Timlan - Tim32 Language</h1>
+
+<div id="navbar">
+
+</div>
+
+<h2>Timlan</h2>
+
+<p>
+The Timlan Websites are undergoing radical changes. These may take some time. Please put up with it for now.
+</p>
+
+<p>
+Links you will want/need:
+<ul>
+<li><a href="lookup">Timlan Lookup</a> - the Timlan dictionary</li>
+<li><a href="lookup/rss.php">Timlan Lookup Feed</a> - the Timlan dictionary RSS feed</li>
+<li><a href="lookup/fixionary.php">Fixionary</a> - the Timlan fixionary</li>
+<li><a href="/blog/index.php?cat=Timlan">Timlan Blog</a> - the Timlan blog</li>
+<li><a href="lookup/tcpfront.php">TCP</a> - online Timlan parser</li>
+<li><a href="docs/timlan_latest/fonti.txt">Fonti</a> - the Timlan phonology</li>
+<li><a href="docs/timlan_latest/speedyintro.txt">Speedy Intro</a> - the Timlan quick reference</li>
+<li><a href="docs/timlan_latest/tools">Tools</a> - some Timlan tools. Some might work</li>
+<li><a href="wiki">Wiki</a> - the (rather sparse) Tim32 documentation wiki</li>
+</p>
+
+<p>
+Feel very free to get in contact if you are interested in helping develop, using, or learning Timlan. You can get in contact by numerous means:
+<ul>
+<li>Timlan Blog - Leave comments on relevent posts in the blog</li>
+<li>IRC - Find us on IRC, tim32.org:6667, #tim32, and get someone to voice you</li>
+<li>E-Mail - we might have a timlan e-mail address at some point</li>
+</p>
+
+<h2 style="color: #222222">Fun <s>New</s> Legal Notice</h2>
+
+<p style="text-size: 8; color: #222222">
+All copyrightable material as part of Timlan documentation and reference guides by Tim32 is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported Licence (<a style="text-size: 8; color: #222222" href="http://creativecommons.org/licenses/by-sa/3.0/">http://creativecommons.org/licenses/by-sa/3.0/</a>). The full licence text is available at <a style="text-size: 8; color: #222222" href="http://creativecommons.org/licenses/by-sa/3.0/legalcode">http://creativecommons.org/licenses/by-sa/3.0/legalcode</a>. Some word definitions are taken from Wiktionary - many of these have been modified. Please visit the page for the corresponding English word at <a style="text-size: 8; color: #222222" href="http://en.wiktionary.org/">http://en.wiktionary.org/</a> for a list of authors.
+</p>
+
+</body>
+</html>
